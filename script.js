8912ab8498446bdfb3460fe087bfdf8792d479bd
Aman Kumar <96618245+Aman123x@users.noreply.github.com>
2023-10-26 00:17:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function volume_sphere() {
     //Write your code here
-  
+	event.preventDefault();
+	const radiusInput=document.getElementById("radius");
+	const radius=radiusInput.value;
+	if(!isNaN(radius)){
+		const volume=(4/3)*Math.PI*Math.pow(radius,3);
+		const volumeInput=document.getElementById("volume");
+		volumeInput.value=volume.toFixed(2);
+	}
+	else {
+    // If the input is not a valid number, display an error message
+    alert('Please enter a valid number for the radius.');
+  }
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
